b8a158c4f57ca0f9e0c20495e5d830b81d41f9c1
Alistair Smith <alistairsmith01@gmail.com>
2021-10-16 15:28:18 +0100

    chore: remove adminOnly

--- packages/core/src/utils/inhibitors.ts
+++ packages/core/src/utils/inhibitors.ts
@@ -1,4 +1,8 @@
 import {CommandInteraction} from 'discord.js';
 import {Inhibitor} from '../types/inhibitors';
 
-export const adminOnly: Inhibitor = (interaction: CommandInteraction) => String(interaction.memberPermissions?.has('ADMINISTRATOR')).toLowerCase() === 'true';
+export function role(name: string): Inhibitor {
+    return interaction => {
+        // todo: impl
+    }
+}
