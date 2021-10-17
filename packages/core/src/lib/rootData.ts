e3fcdc776db891d543923e84c62795aadaef1714
Bereket Semagn <semagnbereket@gmail.com>
2021-10-17 15:53:09 -0400

    disable no unsafe assignment in rootData

--- packages/core/src/lib/rootData.ts
+++ packages/core/src/lib/rootData.ts
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {readFileSync} from 'fs';
 import {dirname, join} from 'path';
 
 export const rootData = () => {
 	const cwd = process.cwd();
+	/* eslint-disable @typescript-eslint/no-unsafe-argument */
+	/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 	const file = JSON.parse(readFileSync(join(cwd, 'package.json'), 'utf8'));
 	return dirname(join(cwd, file.main));
 };
