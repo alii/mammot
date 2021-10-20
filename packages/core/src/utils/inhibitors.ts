f902ff23100b635931c9cbb648e06241d79e95c2
Bereket Semagn <semagnbereket@gmail.com>
2021-10-19 20:03:01 -0400

    feat: role inhibitor

--- packages/core/src/utils/inhibitors.ts
+++ packages/core/src/utils/inhibitors.ts
@@ -1,5 +1,15 @@
 import {Inhibitor} from '../types/inhibitors';
-
+import {GuildMember} from 'discord.js';
 export function channel(id: string): Inhibitor {
 	return interaction => interaction.channel?.id === id;
+}
+
+export function role(id: string): Inhibitor {
+	return interaction => {
+		if (interaction.member instanceof GuildMember) {
+			return Boolean(interaction.member.roles.cache.get(id));
+		}
+
+		return false;
+	};
 }
