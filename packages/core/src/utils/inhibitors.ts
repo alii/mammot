6ff72d966192eed2d807d4f328863b168e2382cd
Bereket Semagn <semagnbereket@gmail.com>
2021-10-21 17:22:51 -0400

    feat: user, guildOnly, & dmOnly default inhibitors

--- packages/core/src/utils/inhibitors.ts
+++ packages/core/src/utils/inhibitors.ts
@@ -1,6 +1,20 @@
 import {Inhibitor} from '../types/inhibitors';
 import {GuildMember} from 'discord.js';
 
+export function user(id: string | string[]): Inhibitor {
+	return interaction => {
+		if (Array.isArray(id)) {
+			return id.includes(interaction.user.id);
+		}
+
+		if (typeof id === 'string') {
+			return interaction.user.id === id;
+		}
+
+		return false;
+	};
+}
+
 export function channel(id: string): Inhibitor {
 	return interaction => interaction.channel?.id === id;
 }
@@ -8,9 +22,17 @@ export function channel(id: string): Inhibitor {
 export function role(id: string): Inhibitor {
 	return interaction => {
 		if (interaction.member instanceof GuildMember) {
-			return Boolean(interaction.member.roles.cache.get(id));
+			return Boolean(interaction.member.roles.cache.has(id));
 		}
 
 		return false;
 	};
+}
+
+export function guildOnly(): Inhibitor {
+	return interaction => interaction.channel?.type === 'GUILD_TEXT';
+}
+
+export function dmOnly(): Inhibitor {
+	return interaction => interaction.channel?.type === 'DM';
 }
