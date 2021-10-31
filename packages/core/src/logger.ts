4346a19bfee2a6357b4dc519424e136a9c2fcafc
andrew <andrewnijmeh1@gmail.com>
2021-10-31 19:29:21 -0400

    feat(add logging to packages/core/src)

---
+++ packages/core/src/logger.ts
@@ -0,0 +1,32 @@
+import * as c from 'colorette';
+
+export class Logger {
+  static get(name: string) {
+    const logger = new Logger(name);
+    return logger.info;
+  }
+
+  public readonly name;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(...args: unknown[]) {
+        console.info(this.prefix(c.blue, 'info'), ...args);
+  }
+  
+  success(...args: unknown[]) {
+        console.log(this.prefix([c.green], 'success'), ...args);
+  }
+  error(...args: unknown[]) {
+        console.log(this.prefix(c.red, 'error'), ...args);
+  }
+
+  private prefix(color: c.Color | c.Color[], text: string) {
+    const colored = Array.isArray(color)
+            ? color.reduce((all, c) => c(all), text)
+            : color(text);
+    return c.bold(`${new Date().toLocaleDateString()} [${this.name} • ${colored}:]`);
+  }
+}
